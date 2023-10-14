ac2410322b027988301220044d445b8e9b4934a7
Lil-Young <thddyddls1@naver.com>
2023-10-14 21:51:41 +0900

    update

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from typing import Union
+from enum import Enum
 
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
+    if model_name is ModelName.lenet:
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"model_name": model_name, "message": "Have some residuals"}
