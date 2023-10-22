3cdf9e4844004f603baa7f086133d59bfc1aa5e0
Lil-Young <thddyddls1@naver.com>
2023-10-22 14:47:58 +0900

    update

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
-@app.post("/items/{item_id}")
-async def create_item(item_id: int, name: str, description: str | None = None, price: float | None=None , tax: float | None = None, q: str | None = None):
-    result = {"item_id": item_id, "name": name, "description": description, "price": price, "tax": tax }
-    if q:
-        result.update({"q": q})
-    return result
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserInDB(BaseModel):
+    username: str
+    hashed_password: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    print(user_saved.hashed_password)
+    return user_saved
