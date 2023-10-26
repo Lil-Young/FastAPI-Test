de40fb2bf371f21a8cebd59cff9e7507f2d2d3b6
Lil-Young <thddyddls1@naver.com>
2023-10-26 14:03:53 +0900

    update

--- 2. Query_Parameters/3. Query_parameter_type_conversion.py
+++ 2. Query_Parameters/3. Query_parameter_type_conversion.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+'''
+url로
+http://127.0.0.1:8000/items/foo?short=1
+http://127.0.0.1:8000/items/foo?short=True
+http://127.0.0.1:8000/items/foo?short=true
+http://127.0.0.1:8000/items/foo?short=on
+http://127.0.0.1:8000/items/foo?short=yes
+쓸 수 있다.
+'''
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: str | None = None, short: bool = False): #
