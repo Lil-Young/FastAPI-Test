5af945bb25ba23da39157301e38c11f9ee34ab91
Lil-Young <thddyddls1@naver.com>
2023-11-02 15:16:25 +0900

    update

--- 3. Request_Body/1. Request_body+Path_Parameters.py
+++ 3. Request_Body/1. Request_body+Path_Parameters.py
@@ -12,7 +12,7 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.put("/items/{item_id}/{dssdffd}")
+@app.put("/items/{item_id}")
 async def create_item(item_id: int, item: Item):
     '''**item.dict()에서 ** 는 딕셔너리의 언패킹 연산자이다.
     이 연산자는 딕셔너리 내의 모든 키-값 쌍을 분리하여 함수 호출 또는 다른 딕셔너리에 전달하는데 사용된다. '''
